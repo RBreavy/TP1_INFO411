bfbfaef5364eebdecceadca97a007816038371ac
viko73 <vincent.larochette@free.fr>
2025-03-14 14:52:41 +0100

    ajout tous les fichier

---
+++ sql/feed_base.sql
@@ -0,0 +1,5 @@
+INSERT INTO utilisateur(`nom`, `prenom`, `mail`, `mdp`) 
+VALUES ('VILNET', 'raphael', 'raphael.vilnet@free.fr', 'viln');
+
+INSERT INTO utilisateur(`nom`, `prenom`, `mail`, `mdp`) 
+VALUES ('VILNET', 'raphael', 'raphael.vilnet@free.fr', 'viln');
